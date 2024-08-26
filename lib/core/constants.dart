a678dfca2da2a37c39cbd37b8069c63be72b66e2
LITS DOO <lits2018@LITSs-MacBook-Pro.local>
2024-08-26 11:19:27 +0200

    Custom text form field

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,1 +1,5 @@
 const String serverError = 'Can not reach the server';
+const int passwordMaxLength = 16;
+const int emailMaxLength = 256;
+const int nameMaxLength = 24;
+const int passwordMinLength = 8;
